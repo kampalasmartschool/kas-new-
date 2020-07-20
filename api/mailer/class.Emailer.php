a38bca43b5bdd8f09e9878ed3fd1fb362fdd10c5
usmansafdaru107 <67827652+usmansafdaru107@users.noreply.github.com>
2020-07-20 19:43:24 +0500

    Update class.Emailer.php

--- api/mailer/class.Emailer.php
+++ api/mailer/class.Emailer.php
@@ -31,46 +31,53 @@
 		}
 
 		public function send($subject){
-			// $header = "From: Kampala Smart School <noreply@kampalasmartschool.com> \r\n";
-			// $header .="Reply-To: Mugarura Emmanuel <admin@kampalasmartschool.com>";
-         	// $header .= "MIME-Version: 1.0\r\n";
-         	// $header .= "Content-type: text/html\r\n";
-         	// //var_dump($this->recipients);
-         	// $to=implode(",",$this->recipients);
+			try {
+				// $header = "From: Kampala Smart School <noreply@kampalasmartschool.com> \r\n";
+				// $header .="Reply-To: Mugarura Emmanuel <admin@kampalasmartschool.com>";
+				// $header .= "MIME-Version: 1.0\r\n";
+				// $header .= "Content-type: text/html\r\n";
+				// //var_dump($this->recipients);
+				// $to=implode(",",$this->recipients);
 
-			// mail ($to,$subject,$this->template,$header);
-			// $this->recipients=[];
+				// mail ($to,$subject,$this->template,$header);
+				// $this->recipients=[];
 
-			date_default_timezone_set('Etc/UTC');
-			$mail = new PHPMailer;
-			$mail->isSMTP();
-			$mail->SMTPDebug = 3;
-			$mail->Debugoutput = 'html';
-			$mail->Host = 'smtp.gmail.com';
-			$mail->Port = 25; 
-			$mail->SMTPOptions = array(
-			    'ssl' => array(
-			        'verify_peer' => false,
-			        'verify_peer_name' => false,
-			        'allow_self_signed' => true
-			    )
-			    );
-			$mail->SMTPSecure = 'tls';
-			$mail->SMTPAuth = true;
-			$mail->Username = $this->credentials['username'];
-			$mail->Password = $this->credentials['password'];
-			$mail->setFrom($this->sender['email'], $this->sender['name']);
+				date_default_timezone_set('Etc/UTC');
+				$mail = new PHPMailer;
+				$mail->isSMTP();
+				$mail->SMTPDebug = 0;
+				$mail->Debugoutput = 'html';
+				$mail->Host = 'smtp.gmail.com';
+				$mail->Port = 25; 
+				$mail->SMTPOptions = array(
+					'ssl' => array(
+						'verify_peer' => false,
+						'verify_peer_name' => false,
+						'allow_self_signed' => true
+					)
+					);
+				$mail->SMTPSecure = 'tls';
+				$mail->SMTPAuth = true;
+				$mail->Username = $this->credentials['username'];
+				$mail->Password = $this->credentials['password'];
+				$mail->setFrom($this->sender['email'], $this->sender['name']);
 
-			foreach ($this->recipients as $recipient){
-				$mail->addAddress($recipient['email'], $recipient['name']);
+				foreach ($this->recipients as $recipient){
+					$mail->addAddress($recipient['email'], $recipient['name']);
+				}
+				//Set the subject line
+				$mail->Subject = $subject;
+				$mail->msgHTML($this->template);
+				$mail->AltBody = $this->txt_body;
+				set_time_limit(300);
+				$mail->send();
+				$this->recipients=array();
+				return true;
+			} catch (\Throwable $th) {
+				//throw $th;
+				return false;
 			}
-			//Set the subject line
-			$mail->Subject = $subject;
-			$mail->msgHTML($this->template);
-			$mail->AltBody = $this->txt_body;
-			set_time_limit(300);
-			$mail->send();
-			$this->recipients=array();
+			
 		}
 	}
 ?>
