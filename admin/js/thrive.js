4d288496e93a9d4f97754691c6bde526d3581116
usmansafdaru107 <67827652+usmansafdaru107@users.noreply.github.com>
2020-07-22 03:29:20 +0500

    Update thrive.js

--- admin/js/thrive.js
+++ admin/js/thrive.js
@@ -10,23 +10,30 @@ function getAllThrive() {
     };
     $.ajax(settings).success(function (response) {
         $('.tutorList').html("");
+        console.log(response);
         $.each(response.data, function (key, value) {
             var typeyy ="";
-            if(value.type=="1" || value.type==1){
+            if(value.sponsor_type=="1" || value.sponsor_type==1){
                 typeyy="School";
+            }else{
+                typeyy="Parent";
             }
-            else if(value.type=="2" || value.type==2) {
-                typeyy="Mentor";
-            }
-            else if(value.type=="3" || value.type==3) {
-                typeyy="Student Support Program";
-            }
-            else if(value.type=="4" || value.type==4) {
-                typeyy="Student Pathway Program";
-            }
-            var tableData = "<tr><td>" + value.full_names
+           
+            // if(value.type=="1" || value.type==1){
+            //     typeyy="School";
+            // }
+            // else if(value.type=="2" || value.type==2) {
+            //     typeyy="Mentor";
+            // }
+            // else if(value.type=="3" || value.type==3) {
+            //     typeyy="Student Support Program";
+            // }
+            // else if(value.type=="4" || value.type==4) {
+            //     typeyy="Student Pathway Program";
+            // }
+            var tableData = "<tr><td>" + value.sponsor_name
              + "</td><td>" 
-             + value.location + "</td>" + 
+             + value.country + "</td>" + 
              "<td>"+value.email+"</td><td>"+value.phone+"</td><td>"+typeyy+"</td></tr>";
             $('.tutorList').append(tableData);
         });
