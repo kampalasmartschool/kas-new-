3b8b7cebf3c644a49a15fcd6f70900731bd031d3
usmansafdaru107 <67827652+usmansafdaru107@users.noreply.github.com>
2020-07-25 01:49:10 +0500

    Create messages.php

---
+++ admin/messages.php
@@ -0,0 +1,105 @@
+<script src="js/thrive.js"></script>
+		<div class="notication alert alert-success"></div>
+		
+		<div class="panel panel-default">
+		
+		<div class="panel-heading">
+		
+		<h3 class="panel-title">All Messages Requests</h3>
+		
+		
+		</div>
+		
+		<!-- BOOK A SMART TUTOR REQUESTS -->
+		<div class="panel-body">
+		<div class="row">
+		</div>
+		<br/>
+		<h4 class="page-header">
+		BOOK A SMART TUTOR REQUESTS
+		</h4>
+		<div class="row">
+		<table class="table table-hover table-responsive">
+			<thead>
+                <tr>
+                <td>Names</td>
+                <td>Location</td>
+                <td>email</td>
+                <td>phone</td>
+                <td>class</td>
+                <td>Curriculum</td>
+			</tr></thead>
+			<tbody class="book_a_tutor">
+                            <script>
+                            getAllMessages();
+                            </script>
+                        <tbody>	
+		</table>
+		</div>
+		</div>
+
+		<!-- BECOME A SMART TUTOR -->
+		<div class="panel-body">
+		<div class="row">
+		</div>
+		<br/>
+		<h4 class="page-header">
+		BECOME A SMART TUTOR REQUESTS
+		</h4>
+		<div class="row">
+		<table class="table table-hover table-responsive">
+			<thead>
+                <tr>
+                <td>Names</td>
+                <td>Location</td>
+                <td>email</td>
+                <td>phone</td>
+                <td>Qualification</td>
+                <td>Subject</td>
+			</tr></thead>
+			<tbody class="be_tutor">
+                            <script>
+                            // getAllMessages();
+                            </script>
+                        <tbody>	
+		</table>
+		</div>
+		</div>
+
+		<!-- General Messages -->
+		<div class="panel-body">
+		<div class="row">
+		</div>
+		<br/>
+		<h4 class="page-header">
+		SEND A MESSAGE & GET IN TOUCH MESSAGES
+		</h4>
+		<div class="row">
+		<table class="table table-hover table-responsive">
+			<thead>
+                <tr>
+                <td>Names</td>
+                <td>email</td>
+                <td>phone</td>
+                <td>Message</td>
+			</tr></thead>
+			<tbody class="message">
+                            <script>
+                            // getAllMessages();
+                            </script>
+                        <tbody>	
+		</table>
+		</div>
+		</div>
+	
+	
+	
+		</div>
+
+		
+		
+
+        
+             
+	
+
